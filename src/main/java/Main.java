2828e87ec2299c8ee4e79e0c6229d13d4c070f0d
James Caruana <caruana.james22@gmail.com>
2016-04-26 18:17:24 +0200

    Revert "Removed Main"
    This reverts commit bde8da360a5478e567100dbbdc494169c94d1bf5.

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author James
+ */
+public class Main {
+    
+    public String concatenate(String one, String two){
+        return one + two;
+    }
+    
+}
