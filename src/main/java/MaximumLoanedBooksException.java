10ad04996173c19708cddba6626fd305fa3ee7b2
James Caruana <caruana.james22@gmail.com>
2016-04-27 18:17:24 +0200

    Uploaded MaximumLoanedBooksException

---
+++ src/main/java/MaximumLoanedBooksException.java
@@ -0,0 +1,20 @@
+public class MaximumLoanedBooksException extends Exception {
+    public MaximumLoanedBooksException() {
+    }
+
+    public MaximumLoanedBooksException(String message) {
+        super(message);
+    }
+
+    public MaximumLoanedBooksException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MaximumLoanedBooksException(Throwable cause) {
+        super(cause);
+    }
+
+    public MaximumLoanedBooksException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
