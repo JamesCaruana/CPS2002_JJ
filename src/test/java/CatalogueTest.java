c03e068025b30bb474bdd6e1bc004d05f031774e
James Caruana <caruana.james22@gmail.com>
2016-04-29 11:00:20 +0200

    Catalogue Upload #4

--- src/test/java/CatalogueTest.java
+++ src/test/java/CatalogueTest.java
@@ -11,14 +11,15 @@ import static org.junit.Assert.*;
 public class CatalogueTest {
 
     static Catalogue c;
-    static Book b0, b1, b2;
-
+    
+    private Book b2; 
+    
     @Before
     public void setUp() throws Exception {
-        c = new Catalogue();
         //b0 = new Book(0,"A Game Of Thrones" , "George R.R. Martin",Genre.FANTASY,1996,1);
         //b1 = new Book(1,"A Clash Of Kings" , "George R.R. Martin",Genre.FANTASY,1999,1);
-        //b2 = new Book(2,"The Hitchhiker's Guide to the Galaxy" , "Douglas Adams",Genre.SCIENCE_FICTION,1979,1);
+         b2 = new Book(2,"The Hitchhiker's Guide to the Galaxy" , "Douglas Adams",Genre.SCIENCE_FICTION,1979,1);
+        c = new Catalogue();
     }
 
     @After
@@ -28,7 +29,7 @@ public class CatalogueTest {
 
     @Test
     public void testGetBookList() throws Exception {
-        Vector<Book> b =  new Vector<Book>();
+        Vector<Book> b = new Vector<Book>();
         Assert.assertEquals(b, c.getBookList());
     }
     
