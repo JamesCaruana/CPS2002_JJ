48ac74a053185835a668689008db3e3d8553df74
James Caruana <caruana.james22@gmail.com>
2016-06-06 23:24:01 +0200

    Re-upload

--- src/main/java/CPS2002Assignment/Book.java
+++ src/main/java/CPS2002Assignment/Book.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.Vector;
 
 public class Book {
+
     private int isbn;
     private long id;
     private String title;
@@ -16,9 +17,8 @@ public class Book {
     private User loanee;
     private Vector<Observer> vo = new Vector<Observer>(); // interested users --> OBSERVER ADDITION
 
-
     // Create a Book
-    public Book(long bookId, int isbn ,String title, String author, Genre genre, int year, int edition){
+    public Book(long bookId, int isbn, String title, String author, Genre genre, int year, int edition) {
         this.title = title;
         this.author = author;
         this.genre = genre;
@@ -29,28 +29,28 @@ public class Book {
         this.loanee = null;
         this.id = bookId;
     }
-    
+
     // getters
     public String getTitle() {
         return title;
     }
-    
+
     public String getAuthor() {
         return author;
     }
-    
+
     public Genre getGenre() {
         return genre;
     }
-    
+
     public int getYear() {
         return year;
     }
-    
+
     public int getEdition() {
         return edition;
     }
-    
+
     public int getISBN() {
         return isbn;
     }
@@ -67,7 +67,7 @@ public class Book {
         return id;
     }
 
-    public Vector<Observer> getObserverVector(){
+    public Vector<Observer> getObserverVector() {
         return vo;
     }
 
@@ -75,23 +75,23 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public void setAuthor(String author) {
         this.author = author;
     }
-    
+
     public void setGenre(Genre genre) {
         this.genre = genre;
     }
-    
+
     public void setYear(int year) {
         this.year = year;
     }
-    
+
     public void setEdition(int edition) {
         this.edition = edition;
     }
-    
+
     public void setISBN(int isbn) {
         this.isbn = isbn;
     }
@@ -100,46 +100,46 @@ public class Book {
         this.loanee = loanee;
     }
 
-    public void setLoanDate(String date , String time) throws Exception{
+    public void setLoanDate(String date, String time) throws Exception {
         String format = "dd/MM/yyyy hh:mm a";
         SimpleDateFormat sdf = new SimpleDateFormat(format);
 
-        try{
+        try {
             loanDate = sdf.parse(date + " " + time);   //parse loanDate
-        }catch(Exception e){
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public void setLoanDate(){
+    public void setLoanDate() {
         loanDate = new Date();
     }
 
-    public void setLoanDate(Object o){loanDate = null; }
+    public void setLoanDate(Object o) {
+        loanDate = null;
+    }
 
 
     /* OBSERVER */
-
-    public void addObserver(Observer observer){
+    public void addObserver(Observer observer) {
         vo.add(observer);
     }
 
-    public void removeObserver(Observer observer){
+    public void removeObserver(Observer observer) {
         vo.remove(observer);
     }
 
-    public User notifyObservers(){
+    public User notifyObservers() {
         int newPos = 0;
         User ret = null;
         for (Observer observer : vo) { //send new position to each observer in vector
-            User temp = observer.update(this,newPos);
-            if(temp != null && ret == null){
+            User temp = observer.update(this, newPos);
+            if (temp != null && ret == null) {
                 ret = temp;
             }
             newPos++; //increment position
         }
         return ret;
     }
-
 
 }
